0dcd683a5366e9f08044afc66e898e2d255d0917
vrizawahyu22 <vrizawahyu22@student.ub.ac.id>
2018-07-03 10:41:23 +0700

    Kelupaan

--- application/controllers/cAdmin.php
+++ application/controllers/cAdmin.php
@@ -57,16 +57,16 @@ class cAdmin extends CI_Controller {
 
 	public function tambahPIC()
 	{
-		if (isset($_POST['submit']))
-		{
-			$NIK = $this->input->post('NIK');
-			$namaPIC = $this->input->post('Nama PIC');
-			$password = $this->input->post('Password');
-			$divisi = $this->input->post('Divisi');
-			$jabatan = $this->input->post('Jabatan');
-			$tahunMasuk = $this->input->post('Tahun Masuk');
-			$jumlahPengecekan = 
-		}
+		// if (isset($_POST['submit']))
+		// {
+		// 	$NIK = $this->input->post('NIK');
+		// 	$namaPIC = $this->input->post('Nama PIC');
+		// 	$password = $this->input->post('Password');
+		// 	$divisi = $this->input->post('Divisi');
+		// 	$jabatan = $this->input->post('Jabatan');
+		// 	$tahunMasuk = $this->input->post('Tahun Masuk');
+		// 	$jumlahPengecekan = 
+		// }
 	}
 
 	public function validasiTambahPIC()
