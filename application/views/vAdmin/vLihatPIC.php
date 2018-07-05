b214c0853f1fb596ee213b3b714a637af0ba30e5
sarahyulievn <sarahyulies@gmail.com>
2018-07-05 14:48:34 +0700

    pic

--- application/views/vAdmin/vLihatPIC.php
+++ application/views/vAdmin/vLihatPIC.php
@@ -6,22 +6,18 @@
 <!------ Include the above in your HEAD tag ---------->
 
 </head>
+
 <body>
-
-<header> </header>
-
-<div class="container">
-
+<div class="container" style="margin-top: 30px">
         <div class="row">
-            
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                     <div></div>
-                        <h3 class="panel-title">PIC</h3>
+                        <h3 class="panel-title" style="text-align: center;">DAFTAR PIC</h3>
                         <div class="pull-right">
                         </div>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" style="padding: 0px">
                         
                     </div>
                     <table class="table table-hover" id="dev-table">
@@ -30,11 +26,11 @@
                             <tr>
                                 <th>NIK</th>
                                 <th>Nama PIC</th>
+                                <th>Password</th>
                                 <th>Divisi</th>
                                 <th>Jabatan</th>
                                 <th>Tahun Masuk</th>
                                 <th>Jumlah Pengecekan</th>
-                                <th>Jabatan</th>
                                 <th>Edit</th>
                                 <th>Hapus</th>
                             </tr>
@@ -49,16 +45,18 @@
                                 <td>kilgore</td>
                                 <td>kilgore</td>
                                 <td>
-                                <div class="container-btn-edit">
-                                    <button class="btn btn-primary">
-                                    <span>
+                                
+                                    <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
                                     Edit
-                                    </span>
                                     </button>
-                                    </div>
+                                    
                                 </td>
                                 <td>
-                                    <button type="button" class="btn btn-danger">Hapus</button>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
                                 </td>
                             </tr>
                             <tr>
@@ -70,16 +68,18 @@
                                 <td>kilgore</td>
                                 <td>kilgore</td>
                                 <td>
-                                <div class="container-btn-edit">
-                                    <button class="btn btn-primary">
-                                    <span>
+                                
+                                    <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
                                     Edit
-                                    </span>
                                     </button>
-                                    </div>
+                                    
                                 </td>
                                 <td>
-                                    <button type="button" class="btn btn-danger">Hapus</button>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
                                 </td>
                             </tr>
                             <tr>
@@ -91,23 +91,115 @@
                                 <td>kilgore</td>
                                 <td>kilgore</td>
                                 <td>
-                                <div class="container-btn-edit">
-                                    <button class="btn btn-primary">
-                                    <span>
+                                <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
                                     Edit
-                                    </span>
                                     </button>
-                                    </div>
                                 </td>
                                 <td>
-                                    <button type="button" class="btn btn-danger">Hapus</button>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Holden</td>
+                                <td>Caulfield</td>
+                                <td>penceyreject</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>
+                                <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                    Edit
+                                    </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Holden</td>
+                                <td>Caulfield</td>
+                                <td>penceyreject</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>
+                                <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                    Edit
+                                    </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Holden</td>
+                                <td>Caulfield</td>
+                                <td>penceyreject</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>
+                                <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                    Edit
+                                    </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Holden</td>
+                                <td>Caulfield</td>
+                                <td>penceyreject</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>kilgore</td>
+                                <td>
+                                <button type="button" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                    Edit
+                                    </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                        Hapus
+                                    </button>
                                 </td>
                             </tr>
                         </tbody>
+
                     </table>
                 </div>
             </div>
-            
+            <button type="button" class="btn btn-dark" style="margin-left: 83%">
+
+                    <span class="glyphicon glyphicon-user"></span>
+                    Tambah PIC
+                    </button>
         </div>
+
     </div>
+    
+    
     </body>
