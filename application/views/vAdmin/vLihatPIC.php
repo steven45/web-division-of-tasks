ca12bce2f5f25414b3b160313b36fa0b7cec4a5c
sarahyulievn <sarahyulies@gmail.com>
2018-07-11 17:22:23 +0700

    new bgt

--- application/views/vAdmin/vLihatPIC.php
+++ application/views/vAdmin/vLihatPIC.php
@@ -12,17 +12,26 @@
 
       <div class="ui divider"></div>
 
-<table class="ui compact celled definition grey inverted table">
+<table class="ui sortable compact celled definition grey inverted table">
   <thead class="full-width" style="text-align: center; background-color: #35373c">
     <tr>
-      <th>NIK</th>
-      <th>Nama PIC</th>
-      <th>Divisi</th>
-      <th>Jabatan</th>
-      <th>Tahun Masuk</th>
-      <th>Jumlah Pengecekan</th>
+      <th class="sorted ascending">NIK</th>
+      <th class="">Nama PIC</th>
+      <th class="">Divisi</th>
+      <th class="">Jabatan</th>
+      <th class="">Tahun Masuk</th>
+      <th class="">Jumlah Pengecekan</th>
       <th>Edit</th>
-      <th>Status</th>
+      <th>
+        <a class="ui simple dropdown item" style="color: white;">
+        Status
+          <i class="dropdown icon"></i>
+            <div class="menu">
+              <div class="item">Enable</div>
+              <div class="item">Disable</div>
+            </div>
+        </a>
+      </th>
     </tr>
   </thead>
   <tbody>
@@ -39,15 +48,15 @@
         </a>
       </td>
       <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
+          <select name="status" class="ui selection tiny dropdown" style="min-width: 10em;">
             <option value="0">Enable</option>
             <option value="1">Disable</option>
           </select>
       </td>
     </tr>
     <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
+      <td>155150222</td>
+      <td>Barah b Evangelista Simarmata KSKJSJS</td>
       <td>ITO</td>
       <td>2005</td>
       <td>2005</td>
@@ -58,15 +67,15 @@
         </div>
       </td>
       <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
+          <select name="status" class="ui selection tiny dropdown" style="min-width: 10em;">
             <option value="0">Enable</option>
             <option value="1">Disable</option>
           </select>
       </td>
     </tr>
     <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
+      <td>155150223</td>
+      <td>Zika b Evangelista Simarmata KSKJSJS</td>
       <td>ITO</td>
       <td>2005</td>
       <td>2005</td>
@@ -77,15 +86,15 @@
         </div>
       </td>
       <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
+          <select name="status" class="ui selection tiny dropdown" style="min-width: 10em;">
             <option value="0">Enable</option>
             <option value="1">Disable</option>
           </select>
       </td>
     </tr>
     <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
+      <td>155150232</td>
+      <td>Charli b Evangelista Simarmata KSKJSJS</td>
       <td>ITO</td>
       <td>2005</td>
       <td>2005</td>
@@ -96,15 +105,15 @@
         </div>
       </td>
       <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
+          <select name="status" class="ui selection tiny dropdown" style="min-width: 10em;">
             <option value="0">Enable</option>
             <option value="1">Disable</option>
           </select>
       </td>
     </tr>
     <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
+      <td>155150122</td>
+      <td>Sarah b Evangelista Simarmata KSKJSJS</td>
       <td>ITO</td>
       <td>2005</td>
       <td>2005</td>
@@ -115,53 +124,15 @@
         </div>
       </td>
       <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
+          <select name="status" class="ui selection tiny dropdown" style="min-width: 10em;">
             <option value="0">Enable</option>
             <option value="1">Disable</option>
           </select>
       </td>
     </tr>
     <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
-      <td>ITO</td>
-      <td>2005</td>
-      <td>2005</td>
-      <td>2005</td>
-      <td>
-        <div class="ui small blue button">
-            <i class="edit icon"></i>Edit
-        </div>
-      </td>
-      <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
-            <option value="0">Enable</option>
-            <option value="1">Disable</option>
-          </select>
-      </td>
-    </tr>
-    <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
-      <td>ITO</td>
-      <td>2005</td>
-      <td>2005</td>
-      <td>2005</td>
-      <td>
-        <div class="ui small blue button">
-            <i class="edit icon"></i>Edit
-        </div>
-      </td>
-      <td>
-          <select name="status" class="ui search selection tiny dropdown" style="min-width: 10em;">
-            <option value="0">Enable</option>
-            <option value="1">Disable</option>
-          </select>
-      </td>
-    </tr>
-    <tr>
-      <td>155150200</td>
-      <td>Sarah</td>
+      <td>155150122</td>
+      <td>Yuli b Evangelista Simarmata KSKJSJS</td>
       <td>ITO</td>
       <td>2005</td>
       <td>2005</td>
