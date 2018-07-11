edce0e4ab917b5a3ef91494e210f1bceeea4f50b
sarahyulievn <sarahyulies@gmail.com>
2018-07-11 11:16:20 +0700

    ss

--- application/views/vAdmin/vTemplate/vFooterAdmin.php
+++ application/views/vAdmin/vTemplate/vFooterAdmin.php
@@ -1,101 +1,11 @@
-<<<<<<< HEAD
+
 <br><br><br><br><!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
   <div class="ui inverted vertical footer segment">
     <div class="ui center aligned container">
     Copyright © Artajasa 2018
   </div>
 </div>
-=======
-  <div style="position: relative;">
-  <footer class="sticky-footer">
-      <div class="container">
-        <div class="text-center">
-          <small>Copyright © Artajasa 2018</small>
-        </div>
-      </div>
-    </footer>
-    </div>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Apakah anda yakin ingin keluar?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">Jika anda keluar maka anda harus memasukkan username dan password kembali</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Kembali</button>
-            <a class="btn btn-primary" href="<?php echo site_url('admin/keluar'); ?>">Keluar</a>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url('assets/home/vendor/jquery/jquery.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/home/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="<?php echo base_url('assets/home/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
-    <!-- Page level plugin JavaScript-->
-    <script src="<?php echo base_url('assets/home/vendor/chart.js/Chart.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/home/vendor/datatables/jquery.dataTables.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/home/vendor/datatables/dataTables.bootstrap4.js'); ?>"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url('assets/home/js/sb-admin.min.js'); ?>"></script>
-    <!-- Custom scripts for this page-->
-    <script src="<?php echo base_url('assets/home/js/sb-admin-datatables.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/home/js/sb-admin-charts.min.js'); ?>"></script>
-
-
-
-
-
-
-    <!-- JS punya sarah -->
-
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/animsition/js/animsition.min.js'); ?>"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/bootstrap/js/popper.js'); ?>"></script>
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/select2/select2.min.js'); ?>"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/daterangepicker/moment.min.js'); ?>"></script>
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/daterangepicker/daterangepicker.js'); ?>"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/vendor/countdowntime/countdowntime.js'); ?>"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo base_url('assets/formT_PIC/js/main.js'); ?>"></script>
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="<?php echo base_url('https://www.googletagmanager.com/gtag/js?id=UA-23581568-13'); ?>"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-23581568-13');
-</script>
-
-
-<!-- penutu JS Sarah -->
-
-
-
-
->>>>>>> 132d7495c48ad83b13058064e8237d4f1d48bec5
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
--- application/views/vAdmin/vTemplate/vHeaderAdmin.php
+++ application/views/vAdmin/vTemplate/vHeaderAdmin.php
@@ -11,7 +11,7 @@
     <?php echo $judul; ?>
   </title>
 </head>
-<<<<<<< HEAD
+
 <body style="background-color: #c7eae9">
   <!-- Header -->
     <div class="ui top fixed inverted pointing menu">
@@ -19,7 +19,7 @@
         <img class="ui avatar image" src="img/Artajasa.png">
         Artajasa
       </div>
-      <a class="item" href="<?php echo site_url('admin/beranda'); ?>">
+      <a class="item"">
         <div class="ui icon">
           <i class="home icon"></i>
           Dashboard</div>
@@ -38,37 +38,6 @@
             <div class="item">Absensi PIC</div>
       </div>
       </a>
-=======
-
-<body class="fixed-nav sticky-footer bg-dark" id="page-top">
-  <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-   
-    <a class="navbar-brand" href="index.html"><img src="<?php echo base_url('assets/images/Artajasa.png'); ?> " style="border-radius: 50%; height: 40px; width: 40px; margin: 10px;">Artajasa</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="<?php echo site_url('admin/beranda'); ?>">
-            <i class="fa fa-home"></i>
-            <span class="nav-link-text">Dashboard</span>
-          </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Checklist">
-          <a class="nav-link" href="<?php echo site_url('admin/checklist'); ?>">
-            <i class="fa fa-check-square"></i>
-            <span class="nav-link-text">Checklist</span>
-          </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="PIC">
-          <a class="nav-link" href="<?php echo site_url('admin/pic'); ?>">
-            <i class="fa fa-users"></i>
-            <span class="nav-link-text">PIC</span>
-          </a>
-        </li>
->>>>>>> 132d7495c48ad83b13058064e8237d4f1d48bec5
         
       <div class="right menu">
       <a class="item">
