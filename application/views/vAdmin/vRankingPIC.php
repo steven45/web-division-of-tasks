6c634a0c3f2aa761de966b33ccf4a386ecf2bbce
vrizawahyu22 <vrizawahyu22@student.ub.ac.id>
2018-08-13 14:54:09 +0700

    anjs

--- application/views/vAdmin/vRankingPIC.php
+++ application/views/vAdmin/vRankingPIC.php
@@ -2,6 +2,10 @@
 <div class="column">
 	<div class="ui segment" style="border-radius: 1.285714rem">
 		<canvas id="myChart"></canvas>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Semantic-UI/semantic.min.css'); ?>">
+          <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Semantic-UI/data-paginate.css'); ?>">
+          <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pagination.min.css'); ?>">
+        <link rel="icon" type="image/png" href= "<?php echo base_url('assets/images/icons/favicon.ico'); ?> "/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script>
 
@@ -64,10 +68,42 @@
         <form method="POST" action="<?php echo site_url('admin/penjadwalan'); ?>">
             <div class="ui input left icon">
                 <i class="calendar icon"></i>
-                <input type="date" value="<?php echo date('20y-m-d'); ?>" name="tanggal">
-              </div>
+                <input type="date" value="<?php echo date('20y-m-d'); ?>" name="date0">
+            </div>
+            -
+            <div class="ui input left icon">
+                <i class="calendar icon"></i>
+                <input type="date" value="<?php echo date('20y-m-d'); ?>" name="date1">
+            </div>
             <button class="ui small green button">Jadwalkan Sekarang! </button>
         </form>
+        <?php for ($i=0; $i < count($alert); $i++) { ?>
+            <?php if ($alert[$i]['hasil'] == 'gagal') { ?>
+                <div class="ui negative message">
+                  <i class="close icon"></i>
+                  <div class="header">
+                    Tanggal <?php echo $alert[$i]['tanggal']; ?> Gagal Dijadwalkan!
+                  </div>
+                  <p>Silahkan lihat absensi PIC yang bertugas.</p>
+                </div>
+            <?php } else if($alert[$i]['hasil'] == 'benar'){ ?>
+                <div class="ui success message">
+                  <i class="close icon"></i>
+                  <div class="header">
+                    Tanggal <?php echo $alert[$i]['tanggal']; ?> Sukses Dijadwalkan.
+                  </div>
+                  <p>Anda bisa melihat jadwal checklist pada tanggal ini.</p>
+                </div>
+            <?php } else if($alert[$i]['hasil'] == 'kosong'){ ?>
+                <div class="ui negative message">
+                  <i class="close icon"></i>
+                  <div class="header">
+                    Tanggal yang anda masukkan tidak valid !
+                  </div>
+                  <p>Silahkan isi tanggal yang benar.</p>
+                </div>
+            <?php } ?>
+        <?php } ?> 
         </div>
 </div>
 </div>
